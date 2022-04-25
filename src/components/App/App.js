aea1583e15ed06a98667001961c7fcfdbb730138
Ashley Lampard <ashleylampard@live.com>
2022-04-25 14:18:51 +0100

    implemented the usage of this.props to pass info through to business.js elements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,27 @@ import './App.css';
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1005 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 56
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
 // 2 ways to do it - both work
 
 //Option 1:
@@ -26,7 +47,7 @@ function App() {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList/>
+        <BusinessList businesses={businesses}/>
       </div>
   );
 }
